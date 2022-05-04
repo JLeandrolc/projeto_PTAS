e388d29c5373075cc9198e16cb8e8656f48031a2
Jorge Leandro Lima Carvalho <90215418+JLeandrolc@users.noreply.github.com>
2022-05-04 20:39:35 +0000

    Adicionei o PUT e DELETE

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ app.use(express.urlencoded({
 app.get("/", async function(req, res){
   var resultado = await usuario.findAll();
   res.json(resultado);
-})
+});
 
 app.post("/", async function(req, res){
   var resultado = usuario.create(req.body);
   res.json(resultado);
 });
 
+app.put("/:id", async function (req, res){
+  var resultado = usuario.update(req.body,{ where: { id: req.params.id }});
+  res.json(resultado);
+});
+
+app.delete("/", async function(req, res){
+  usuario.destroy({ where: { id: 1 }});
+});
+
 app.listen(3000, function(){
-  console.log("o servidor esta funcionando");
+  console.log("Servidor esta funcionando");
 });
 
