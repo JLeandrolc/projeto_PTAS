2b0e4a5cf7a960a48d7722e9a21a478feae05600
Jorge Leandro Lima Carvalho <90215418+JLeandrolc@users.noreply.github.com>
2022-05-11 20:52:34 +0000

    Adicionei as rotas empresas

--- index.js
+++ index.js
@@ -7,28 +7,54 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/", async function(req, res) {
+app.get("/usuarios", async function(req, res) {
   var resultado = await usuario.findAll();
   res.json(resultado);
 });
 
-app.get("/:id", async function(req, res) {
+app.get("/usuarios/:id", async function(req, res) {
   var resultado = await usuario.findByPk(req.params.id);
   res.json(resultado);
 });
 
-app.post("/", async function(req, res) {
+app.post("/usuarios", async function(req, res) {
   var resultado = usuario.create(req.body);
   res.json(resultado);
 });
 
-app.put("/:id", async function(req, res) {
+app.put("/usuarios/:id", async function(req, res) {
   var resultado = usuario.update(req.body, { where: { id: req.params.id } });
   res.json(resultado);
 });
 
-app.delete("/", async function(req, res) {
+app.delete("/usuarios", async function(req, res) {
   usuario.destroy({ where: { id: 1 } });
+});
+
+// rotas empresas
+
+app.get("/empresas", async function(req, res) {
+  var resultado = await empresa.findAll();
+  res.json(resultado);
+});
+
+app.get("/empresas/:id", async function(req, res) {
+  var resultado = await empresa.findByPk(req.params.id);
+  res.json(resultado);
+});
+
+app.post("/empresas", async function(req, res) {
+  var resultado = empresa.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/empresas/:id", async function(req, res) {
+  var resultado = empresa.update(req.body, { where: { id: req.params.id } });
+  res.json(resultado);
+});
+
+app.delete("/empresas", async function(req, res) {
+  empresa.destroy({ where: { id: 1 } });
 });
 
 app.listen(3000, function() {
