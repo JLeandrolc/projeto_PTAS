529ae401364d5874877ed588afaba1928ac710d5
Jorge Leandro Lima Carvalho <90215418+JLeandrolc@users.noreply.github.com>
2022-05-11 19:55:17 +0000

    Adicionei empresa usando o sequelize

--- index.js
+++ index.js
@@ -7,31 +7,31 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/", async function(req, res){
+app.get("/", async function(req, res) {
   var resultado = await usuario.findAll();
   res.json(resultado);
 });
 
-app.get("/:id", async function (req, res){
+app.get("/:id", async function(req, res) {
   var resultado = await usuario.findByPk(req.params.id);
   res.json(resultado);
 });
 
-app.post("/", async function(req, res){
+app.post("/", async function(req, res) {
   var resultado = usuario.create(req.body);
   res.json(resultado);
 });
 
-app.put("/:id", async function (req, res){
-  var resultado = usuario.update(req.body,{ where: { id: req.params.id }});
+app.put("/:id", async function(req, res) {
+  var resultado = usuario.update(req.body, { where: { id: req.params.id } });
   res.json(resultado);
 });
 
-app.delete("/", async function(req, res){
-  usuario.destroy({ where: { id: 1 }});
+app.delete("/", async function(req, res) {
+  usuario.destroy({ where: { id: 1 } });
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Servidor esta funcionando");
 });
 
